c193c762cee1a10817523cd8f8aaad3e7e976de6
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-09 12:02:23 -0400

    emotion css setup

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,23 +1,23 @@
-//package components
-//about
-//login page
-//profile page 
-//registration
+import React from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import {colors} from '../styleVars'
+const {primaryColor, darkColor, lightColor} = colors;
+const main = css`
+  color: ${primaryColor};
+
+  h1 {
+    font-size: 4rem;
+  }
 
 
+`
 
-// import React, { Component } from 'react'
-
-
-// export default class LandingPage extends Component {
-    
-
-//     render() {
-//         return (
-//             <div>
-                
-//             </div>
-//         )
-//     }
-// }
+export default function LandingPage() {
+  return (
+    <div css={main}>
+      <h1>hello world</h1>
+    </div>
+  )
+}
 
