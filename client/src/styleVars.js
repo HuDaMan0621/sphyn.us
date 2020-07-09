def3cf0beaa21c58b51227eb9187a5d93ab4fd09
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-09 16:22:37 -0400

    initial jsx and styling for header, showcase image, and about section

--- client/src/styleVars.js
+++ client/src/styleVars.js
@@ -1,5 +1,11 @@
 export const colors = {
-  primaryColor: 'red',
-  darkColor: '#333',
-  lightColor: '#fff',
+  primaryColor: '#84a9ac',
+  darkColor: '#204051',
+  secondaryColor: '#3b6978',
+  lightColor: '#e7dfd5',
+}
+
+export const utilities = {
+  borderRadius: '3px',
+  animationSpeed: '0.3s',
 }
