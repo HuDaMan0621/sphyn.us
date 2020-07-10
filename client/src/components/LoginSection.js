8f7d6db11a5ecf8242c69e2120274df58bea08e4
Ying Hu <57694281+HuDaMan0621@users.noreply.github.com>
2020-07-09 20:31:53 -0400

    login route working (#10)
    Looks grrrrreat!

--- client/src/components/LoginSection.js
+++ client/src/components/LoginSection.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'  //
 
-export default function LoginSection() {
-    return (
-        <div>
-            <form>
-                <label htmlFor="loginName"><input className="loginName" placeholder="User Name"></input></label>
-                <label htmlFor="loginPassword"><input className="loginPassword" placeholder="Password"></input></label>
-                <button className="submit" type="submit" >Submit</button>
-            </form>
-        </div>
-    )
+export default class LoginSection extends Component {
+    state = {
+        // first_name: 'this',
+        // last_name: 'this',
+        email: '',
+        // login_name: 'this',
+        login_password: '',
+        // phone_number: '1111111',
+        // address_line_1: 'this',
+        // address_line_2: 'this',
+        // address_line_3: 'this',
+        // city: 'this',
+        // state: 'GA',
+        // zipcode: '11111',
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                this.props.history.push('/customer/:id/profile')
+            })
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <input htmlFor="email" className="email" name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email}></input>
+                    <label htmlFor="loginPassword"><input className="loginPassword" name="login_password" placeholder="Password"onChange={this.handleChange} value={this.state.login_password}></input></label>
+                    <button className="submit" type="submit" >Submit</button>
+                </form>
+            </div>
+        )
+    }
 }
