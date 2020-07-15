91d902b12fe189951890f567e0d5283c0115b203
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-15 11:02:24 -0400

    started restyling of packages section

--- client/src/components/Packages.js
+++ client/src/components/Packages.js
@@ -1,117 +1,88 @@
-import React from 'react';
+import React from "react";
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core';
-import { colors, utilities } from '../styleVars';
-import { Link } from 'react-router-dom';
-
-const { primaryColor, darkColor, secondaryColor, lightColor } = colors;
-const { borderRadius, animationSpeed } = utilities;
-
+import { jsx, css } from "@emotion/core";
+import { colors, utilities } from "../styleVars";
+import { Link } from "react-router-dom";
 
 const packages = css`
-.Packages-wrapper {
+  padding: ${utilities.sectionPadding};
+  .packages-wrap {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-gap: 1rem;
     padding: 0 2rem;
     justify-content: end;
-    
-}
 
     .package {
-        border: 1px solid ${lightColor};
-        border-radius: ${borderRadius};
+      border: 1px solid ${colors.darkColor};
+
+      a {
+        display: block;
+      }
     }
-
-    .heading {
-        background-color: ${darkColor};
-        color: ${lightColor}
-    }
-
-    .price {
-        border-bottom: 1px solid ${lightColor};
-        font-size: 3em;
-    }
-
-    .details {
-        //  background-color: ${lightColor};
-        text-align: left;
-        // color: ${secondaryColor};
-        padding: 1rem .5rem;
-
-        li {
-            padding: .5rem 0;
-        }
-
-
-        
-    }
-
-    h1 {
-        margin: 1rem 0;
-    }
-
-    
-
-`
+  }
+`;
 
 export default function Packages() {
-    return (
-        <div css={packages} >
-            <h1>Packages</h1>
-            <div className='Packages-wrapper'>
-                <div className="package">
-                    <div className="heading">
-                        <h3>Test Package Title</h3>
-                        <p>These are some test package details</p>
-                    </div>
-                    <div className="price">$150</div>
-                    <div className="details">
-                        <ul>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                        </ul>
-                    </div>
-                    <Link to="/login">Returning User Book Now</Link>
-                    <Link to="/register">New User Book Now</Link>
-                </div>
-                <div className="package">
-                    <div className="heading">
-                        <h3>Test Package Title</h3>
-                        <p>These are some test package details</p>
-                    </div>
-                    <div className="price">$300</div>
-                    <div className="details">
-                        <ul>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                        </ul>
-                    </div>
-                    <Link to="/login">Returning User Book Now</Link>
-                    <Link to="/register">New User Book Now</Link>
-                </div>
-                <div className="package">
-                    <div className="heading">
-                        <h3>Test Package Title</h3>
-                        <p>These are some test package details</p>
-                    </div>
-                    <div className="price">$500</div>
-                    <div className="details">
-                        <ul>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                            <li>Test bullet</li>
-                        </ul>
-                    </div>
-                    <Link to="/login">Returning User Book Now</Link>
-                    <Link to="/register">New User Book Now</Link>
-                </div>
-            </div>
-        </div >
-    )
+  return (
+    <div css={packages}>
+      <h3 className="m-heading">Packages</h3>
+      <div className="packages-wrap">
+        <div className="package">
+          <div className="heading">
+            <h3>Package 1</h3>
+          </div>
+          <div className="price">$150</div>
+          <div className="details">
+            <ul>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+            </ul>
+          </div>
+          <div className="links">
+            <Link to="/login">Returning User Book Now</Link>
+            <Link to="/register">New User Book Now</Link>
+          </div>
+        </div>
+        <div className="package">
+          <div className="heading">
+            <h3>Package 2</h3>
+          </div>
+          <div className="price">$150</div>
+          <div className="details">
+            <ul>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+            </ul>
+          </div>
+          <div className="links">
+            <Link to="/login">Returning User Book Now</Link>
+            <Link to="/register">New User Book Now</Link>
+          </div>
+        </div>
+        <div className="package">
+          <div className="heading">
+            <h3>Package 3</h3>
+          </div>
+          <div className="price">$150</div>
+          <div className="details">
+            <ul>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+              <li>Test bullet</li>
+            </ul>
+          </div>
+          <div className="links">
+            <Link to="/login">Returning User Book Now</Link>
+            <Link to="/register">New User Book Now</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
