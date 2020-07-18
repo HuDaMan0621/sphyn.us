142374c59d660ff1c1d4372397c56223ebe68088
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-18 13:13:03 -0400

    iframe models added to demo section

--- client/src/components/layout/Demo.js
+++ client/src/components/layout/Demo.js
@@ -4,32 +4,53 @@ import { jsx, css } from "@emotion/core";
 import { colors, utilities } from "../../styleVars";
 
 const demo = css`
-  .demo {
-    padding: ${utilities.sectionPadding};
-    background: ${colors.darkColor};
-    @media (min-width: 768px) {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-    }
+  background: ${colors.darkColor};
+  height: 500px;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  iframe {
+    width: 100%;
+    height: 100%;
   }
 `;
 
 export default function Demo() {
   return (
     <div css={demo}>
-      <div className="demo">
-        <div>
-          <img src="./images/demo1.jpg" alt="inside of penthouse apartment" />
-        </div>
-        <div>
-          <img src="./images/demo2.jpg" alt="inside of penthouse apartment" />
-        </div>
-        <div>
-          <img src="./images/demo3.jpg" alt="inside of penthouse apartment" />
-        </div>
-        <div>
-          <img src="./images/demo4.jpg" alt="inside of penthouse apartment" />
-        </div>
+      <div>
+        <iframe
+          frameborder="0"
+          allowfullscreen=""
+          allow="xr-spatial-tracking"
+          src="https://my.matterport.com/show/?m=xz4qyaULAhy"
+        ></iframe>
+      </div>
+      <div>
+        <iframe
+          frameborder="0"
+          allowfullscreen=""
+          allow="xr-spatial-tracking"
+          src="https://my.matterport.com/show/?m=xbYh4Vuis2s"
+        ></iframe>
+      </div>
+      <div>
+        <iframe
+          frameborder="0"
+          allowfullscreen=""
+          allow="xr-spatial-tracking"
+          src="https://my.matterport.com/show/?m=K2svdmMGHBG"
+        ></iframe>
+      </div>
+      <div>
+        <iframe
+          frameborder="0"
+          allowfullscreen=""
+          allow="xr-spatial-tracking"
+          src="https://my.matterport.com/show/?m=sqMTt9HSrFa"
+        ></iframe>
       </div>
     </div>
   );
