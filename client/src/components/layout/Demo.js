1db46e2de23c5467384f236704346ebd4f5db47f
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-18 15:27:40 -0400

    demo videos styles, about section text content added

--- client/src/components/layout/Demo.js
+++ client/src/components/layout/Demo.js
@@ -4,16 +4,20 @@ import { jsx, css } from "@emotion/core";
 import { colors, utilities } from "../../styleVars";
 
 const demo = css`
-  background: ${colors.darkColor};
   height: 500px;
-  @media (min-width: 768px) {
+
+  margin: auto;
+  @media (min-width: 600px) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
   }
 
   iframe {
     width: 100%;
-    height: 100%;
+    height: 250px;
+    @media (min-width: 768px) {
+      height: 100%;
+    }
   }
 `;
 
@@ -23,7 +27,7 @@ export default function Demo() {
       <div>
         <iframe
           frameborder="0"
-          allowfullscreen=""
+          allowfullscreen="100%"
           allow="xr-spatial-tracking"
           src="https://my.matterport.com/show/?m=xz4qyaULAhy"
         ></iframe>
@@ -34,22 +38,6 @@ export default function Demo() {
           allowfullscreen=""
           allow="xr-spatial-tracking"
           src="https://my.matterport.com/show/?m=xbYh4Vuis2s"
-        ></iframe>
-      </div>
-      <div>
-        <iframe
-          frameborder="0"
-          allowfullscreen=""
-          allow="xr-spatial-tracking"
-          src="https://my.matterport.com/show/?m=K2svdmMGHBG"
-        ></iframe>
-      </div>
-      <div>
-        <iframe
-          frameborder="0"
-          allowfullscreen=""
-          allow="xr-spatial-tracking"
-          src="https://my.matterport.com/show/?m=sqMTt9HSrFa"
         ></iframe>
       </div>
     </div>
