1980b3ff276ca8a17a1c92b9d8a5c4811c4d78af
hudaman0621 <mrhu828@gmail.com>
2020-07-18 15:38:31 -0400

    0718AM copy where showcase is working

--- client/src/components/Showcase.js
+++ client/src/components/Showcase.js
@@ -16,29 +16,15 @@ export default function Showcase() {
                 setIsLoading(false);
                 setData2(data);
                 console.log('print 1 times? why')
+                console.log(data)
             })
             .catch((error) => console.log("Please Login"));
     }, [])
 
     console.log(galleryInfo)
-    // return (
-    //     <div>
-    //         {isLoading ? <div>loading...</div> : (
-    //             <div>
-    //                 <h1>Showcase</h1>
-    //                 <h1>Display customer information </h1>
-    //                 <QRCode value={`http://localhost:3000/${galleryInfo[0].customer_id}/showcase`} />
-    //                 <div>{<iframe width="853" height="480" src={`${galleryInfo[0].matterport}`} frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe>}</div>
-    //                 {/* {<iframe width="853" height="480" src="https://my.matterport.com/show/?m=CoPaqg84uwx&brand=0" frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe>} */}
-    //                 <GoogleMaps address={galleryInfo[0].address} city={galleryInfo[0].city} state={galleryInfo[0].state} zipcode={galleryInfo[0].zipcode} />
-    //             </div>
-    //         )
-    //         }
-    //     </div >
-    // );
     return (
         <div>
-            {!galleryInfo[0] ? <div><h1>default matterport example</h1></div> : (
+            {isLoading ? <div>loading...</div> : (
                 <div>
                     <h1>Showcase</h1>
                     <h1>Display customer information </h1>
@@ -49,8 +35,24 @@ export default function Showcase() {
                 </div>
             )
             }
+            {/* https://my.matterport.com/show/?m=rvKhT7rSPML */}
         </div >
     );
+    // return (
+    //     <div>
+    //         {!galleryInfo[0] ? <div><h1>Your Next House</h1> <br></br><div>{<iframe width="853" height="480" src={`https://my.matterport.com/show/?m=rvKhT7rSPML`} frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe>}</div></div> : (
+    //             <div>
+    //                 <h1>Showcase</h1>
+    //                 <h1>Display customer information </h1>
+    //                 <QRCode value={`http://localhost:3000/${galleryInfo[0].customer_id}/showcase`} />
+    //                 <div>{<iframe width="853" height="480" src={`${galleryInfo[0].img_url}`} frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe>}</div>
+    //                 {/* {<iframe width="853" height="480" src="https://my.matterport.com/show/?m=CoPaqg84uwx&brand=0" frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe>} */}
+    //                 <GoogleMaps address={galleryInfo[0].address} city={galleryInfo[0].city} state={galleryInfo[0].state} zipcode={galleryInfo[0].zipcode} />
+    //             </div>
+    //         )
+    //         }
+    //     </div >
+    // );
 }
 
 //!! try to fix showcase error when user has no matterport url
