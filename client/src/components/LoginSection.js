587676873f4cba38986a7b0631c5d44698903f75
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-20 13:59:15 -0400

    added back to home link from login

--- client/src/components/LoginSection.js
+++ client/src/components/LoginSection.js
@@ -14,6 +14,9 @@ const login = css`
 
   a {
     color: ${colors.darkColor};
+    &:hover {
+      color: ${colors.primaryColor};
+    }
   }
 
   .m-heading {
@@ -65,7 +68,6 @@ const login = css`
   }
 `;
 
-
 export default class LoginSection extends Component {
   constructor(props) {
     super(props);
@@ -77,44 +79,48 @@ export default class LoginSection extends Component {
   }
 
   handleFormSubmit = (e) => {
-    let customerRole = ''
+    let customerRole = "";
     e.preventDefault();
     axios
       .post("/api/v1/login", {
         ...this.state,
       })
       .then((res) => {
-        this.setState({ data: res.data })
-        if (this.state.email === 'authorized@gmail.com') {
-          customerRole = 'admin';
-          this.props.history.push('/admin/update');
-          console.log('ADMIN')
+        this.setState({ data: res.data });
+        if (this.state.email === "authorized@gmail.com") {
+          customerRole = "admin";
+          this.props.history.push("/admin/update");
+          console.log("ADMIN");
         } else {
           this.props.history.push(`/customer/profile`);
         }
-      })
+      });
   };
 
   handleChange = (e) => {
-    let customerRole = ''
+    let customerRole = "";
     const { name, value } = e.target;
     this.setState({
       [name]: value,
-    })
+    });
   };
 
   analyzePassword = (e) => {
-    const realRegex = new RegExp("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$");
+    const realRegex = new RegExp(
+      "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+    );
     const { name, value } = e.target;
     this.setState({
       [name]: value,
     });
     if (realRegex.test(e.target.value)) {
-      e.target.setCustomValidity('')
+      e.target.setCustomValidity("");
     } else {
-      e.target.setCustomValidity("Password must contain a minimum of eight characters, with at least one letter, one number, and one special character");
+      e.target.setCustomValidity(
+        "Password must contain a minimum of eight characters, with at least one letter, one number, and one special character"
+      );
     }
-  }
+  };
 
   render() {
     return (
@@ -147,7 +153,8 @@ export default class LoginSection extends Component {
             Submit
           </button>
         </form>
-      </div >
+        <Link to="/">Back to home</Link>
+      </div>
     );
   }
 }
