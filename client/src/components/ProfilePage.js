3b137e17102473ef6946daf70cdd263365f890c1
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-20 10:42:06 -0400

    Profile component html edit/cleanup and layour arrangement

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,48 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import QRCode from 'qrcode.react';
-import Logout from './Logout';
-import { Link } from 'react-router-dom';
-import Service from './Service';
-import Order from './Order';
+/** @jsx jsx */
+import React, { useState, useEffect } from "react";
+import { jsx, css } from "@emotion/core";
+import { colors, utilities } from "../styleVars";
+import QRCode from "qrcode.react";
+import Logout from "./Logout";
+import { Link } from "react-router-dom";
+import Service from "./Service";
+
+const profile = css``;
 
 export default function ProfilePage(props) {
-    const [data2, setData2] = useState({ error: " " }); //this is the state for the customer
-    const [isLoading, setIsLoading] = useState(true); //displays loading when user clicks
-    useEffect(() => {
-        fetch(`/api/v1/customer/profile`)
-            .then((data) => data.json())
-            .then((data) => {
-                setData2(data);
-                setIsLoading(false);
-            })
-            .catch((error) => console.log("Please Login"));
-    }, []);
+  const [data2, setData2] = useState({ error: " " }); //this is the state for the customer
+  const [isLoading, setIsLoading] = useState(true); //displays loading when user clicks
 
-    return (
+  useEffect(() => {
+    fetch(`/api/v1/customer/profile`)
+      .then((data) => data.json())
+      .then((data) => {
+        setData2(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log("Please Login"));
+  }, []);
+
+  return (
+    <div css={profile}>
+      {data2.error ? (
         <div>
-            {data2.error ? <div>User Not Authorized. <br />Please <Link to="/login">log in</Link></div> : (
-                <div>
-                    {data2.data.id && <QRCode value={`http://localhost:3000/customer/${data2.data.id}/showcase`} />}
-                    <h1>Profile Page!!!!!!</h1>
-                    <h1>{data2.data.first_name}</h1>
-                    {/* <h1>{data.last_name}</h1> */}
-                    <form onSubmit={e => {
-                        e.preventDefault();
-                    }}>
-                        <Logout />
-                    </form>
-                    <Link to='/booking'>Book Service</Link>
-                    {/* <Link to={`http://localhost:3000/customer/${data2.data.id}/showcase`}>
-                        Showcase
-                    </Link> */}
-                    <Link to="/showcase">the real showcase</Link>
-                    <div>
-                    </div>
-                </div>
-
-            )}
-            <Service />
-            {/* <Order /> */}
+          <h1>User Not Authorized</h1>
+          <h3>
+            Please <Link to="/login">log in</Link>{" "}
+          </h3>
         </div>
-    );
+      ) : (
+        <div>
+          <header>
+            <h1>{data2.data.first_name}</h1>
+            <Logout />
+          </header>
+          <div className="qrcode">
+            {data2.data.id && (
+              <QRCode
+                value={`http://localhost:3000/customer/${data2.data.id}/showcase`}
+              />
+            )}
+          </div>
+          <Link to="/booking">Book Service</Link>
+          <Link to="/showcase">My Showcase</Link>
+        </div>
+      )}
+      <Service />
+    </div>
+  );
 }
--- client/src/components/Service.js
+++ client/src/components/Service.js
@@ -1,30 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 export default function Service() {
-    const [serviceInfo, setServiceInfo] = useState([]);
+  const [serviceInfo, setServiceInfo] = useState([]);
 
+  useEffect(() => {
+    fetch(`/api/v1/customer/services`)
+      .then((data) => data.json())
+      .then((data) => {
+        setServiceInfo(data);
+      })
+      .catch((error) => console.log("Please Login"));
+  }, []);
 
-    useEffect(() => {
-        fetch(`/api/v1/customer/services`)
-            .then(data => data.json())
-            .then(data => {
-                setServiceInfo(data)
-            })
-            .catch(error => console.log('Please Login')
-            )
-    }, [])
-
-    return (
-        <div>
-            <div>Service List:</div>
-            {serviceInfo.map((service, i) => {
-                return (
-                    <div key={i}>
-                        <p><span>{service.nick_name}</span>{service.sq_ft}<span>{service.address}</span><br /></p>
-                        <iframe width="853" height="480" src={service.img_url} frameBorder="0" allowFullScreen allow="xr-spatial-tracking"></iframe>
-                    </div>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div>
+      {serviceInfo.map((service, i) => {
+        return (
+          <div key={i}>
+            <p>
+              <span>{service.nick_name}</span>
+              {service.sq_ft}
+              <span>{service.address}</span>
+              <br />
+            </p>
+            <iframe
+              width="853"
+              height="480"
+              src={service.img_url}
+              frameBorder="0"
+              allowFullScreen
+              allow="xr-spatial-tracking"
+            ></iframe>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
