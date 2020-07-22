0c4b2927bc367bd1dd549abf996aff641899f58f
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-22 10:48:02 -0400

    showcase homes styling complete for mobile

--- client/src/components/ShowcaseFilter.js
+++ client/src/components/ShowcaseFilter.js
@@ -68,7 +68,9 @@ const serviceFilter = css`
       .name {
         color: ${colors.primaryColor};
       }
-    }`;
+    }
+  }
+`;
 
 export default function ShowcaseFilter() {
   const [data2, setData2] = useState({ error: "" }); //this is the state for the customer
@@ -85,23 +87,13 @@ export default function ShowcaseFilter() {
     e.preventDefault();
     console.log(email);
     fetch(`/api/v1/selectserviceslist/${email}`) //!todo
-      // .then(res => {
-      //     if (res.ok === false) { this.props.history.push('/login') }
-      //     return res
-      //     console.log(res)
-      //   })
       .then((data) => data.json())
       .then((data) => {
         setData2(data);
-        // if (!data2.error)
         setFilteredServices(data.Services);
         if (data.Services && data.Services.length == 0) {
           setNoServices(true);
         }
-        console.log(filteredServices);
-        console.log(data);
-        console.log("filteredServices");
-        //how to make a copy of services? and access it
       });
   };
 
@@ -109,13 +101,13 @@ export default function ShowcaseFilter() {
     <>
       {data2.error || noServices ? (
         <>
-          <div>No services for this user</div>
+          <h3>No services for this user</h3>
           <a href="https://sphyn.herokuapp.com/showcase">
             Click here to reload page and re-enter correct email
           </a>
         </>
       ) : (
-        <>
+        <div css={serviceFilter}>
           <form onSubmit={handleSubmit}>
             <input
               htmlFor="email"
@@ -126,23 +118,22 @@ export default function ShowcaseFilter() {
               name="email"
               placeholder="Email address"
             ></input>
-            <button type="submit">Submit</button>
+            `<button type="submit">Submit</button>
           </form>
           <>
             {filteredServices &&
               filteredServices.map((service, i) => {
                 return (
-                  <div key={i}>
+                  <div className="homes" key={i}>
                     <p>
                       <span>{service.nick_name}</span>
-                      {service.sq_ft}
                       <span>{service.address}</span>
                       <br />
                     </p>
                     {!service.img_url ? (
                       <iframe
-                        width="853"
-                        height="480"
+                        width="100%"
+                        height="300"
                         src="https://my.matterport.com/show/?m=rvKhT7rSPML"
                         frameborder="0"
                         allowfullscreen
@@ -150,8 +141,8 @@ export default function ShowcaseFilter() {
                       ></iframe>
                     ) : (
                       <iframe
-                        width="853"
-                        height="480"
+                        width="100%"
+                        height="300"
                         src={service.img_url}
                         frameBorder="0"
                         allowFullScreen
@@ -168,7 +159,7 @@ export default function ShowcaseFilter() {
                 );
               })}
           </>
-        </>
+        </div>
       )}
     </>
   );
