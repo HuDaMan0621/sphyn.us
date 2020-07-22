98e381add286df980d7ac72706305aa87f5fbf39
ChrisKnowsDev <chrisharley81@gmail.com>
2020-07-22 10:35:32 -0400

    Showcase header complete

--- client/src/components/ShowcaseFilter.js
+++ client/src/components/ShowcaseFilter.js
@@ -68,7 +68,7 @@ const serviceFilter = css`
       .name {
         color: ${colors.primaryColor};
       }
-    }`
+    }`;
 
 export default function ShowcaseFilter() {
   const [data2, setData2] = useState({ error: "" }); //this is the state for the customer
@@ -82,52 +82,94 @@ export default function ShowcaseFilter() {
     });
   };
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(email)
-    fetch(`/api/v1/selectserviceslist/${email}`)  //!todo
+    e.preventDefault();
+    console.log(email);
+    fetch(`/api/v1/selectserviceslist/${email}`) //!todo
       // .then(res => {
       //     if (res.ok === false) { this.props.history.push('/login') }
       //     return res
       //     console.log(res)
       //   })
-      .then(data => data.json())
-      .then(data => {
+      .then((data) => data.json())
+      .then((data) => {
         setData2(data);
-        // if (!data2.error) 
-        setFilteredServices(data.Services)
+        // if (!data2.error)
+        setFilteredServices(data.Services);
         if (data.Services && data.Services.length == 0) {
-          setNoServices(true)
+          setNoServices(true);
         }
-        console.log(filteredServices)
-        console.log(data)
-        console.log('filteredServices')
-        //how to make a copy of services? and access it 
-      })
-  }
+        console.log(filteredServices);
+        console.log(data);
+        console.log("filteredServices");
+        //how to make a copy of services? and access it
+      });
+  };
 
   return (
     <>
-      {data2.error || noServices ? <><div>No services for this user</div><a href="https://sphyn.herokuapp.com/showcase">Click here to reload page and re-enter correct email</a></> :
-        (<>
+      {data2.error || noServices ? (
+        <>
+          <div>No services for this user</div>
+          <a href="https://sphyn.herokuapp.com/showcase">
+            Click here to reload page and re-enter correct email
+          </a>
+        </>
+      ) : (
+        <>
           <form onSubmit={handleSubmit}>
-            <input htmlFor="email" className="emailInput" onChange={(e) => setEmail(e.target.value)}
-              value={email} type="email" name="email" placeholder="Email address"></input>
-            <button type="submit">the somewhat working button</button>
+            <input
+              htmlFor="email"
+              className="emailInput"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              name="email"
+              placeholder="Email address"
+            ></input>
+            <button type="submit">Submit</button>
           </form>
           <>
-            {filteredServices && filteredServices.map((service, i) => {
-              return (
-                <div key={i}>
-                  <p><span>{service.nick_name}</span>{service.sq_ft}<span>{service.address}</span><br /></p>
-                  {!service.img_url ? < iframe width="853" height="480" src="https://my.matterport.com/show/?m=rvKhT7rSPML" frameborder='0' allowfullscreen allow="xr-spatial-tracking"></iframe> :
-                    <iframe width="853" height="480" src={service.img_url} frameBorder="0" allowFullScreen allow="xr-spatial-tracking"></iframe>}
-                  <GoogleMaps address={service.address} city={service.city} state={service.state} zipcode={service.zipcode} />
-                </div>
-              )
-            }
-            )}
+            {filteredServices &&
+              filteredServices.map((service, i) => {
+                return (
+                  <div key={i}>
+                    <p>
+                      <span>{service.nick_name}</span>
+                      {service.sq_ft}
+                      <span>{service.address}</span>
+                      <br />
+                    </p>
+                    {!service.img_url ? (
+                      <iframe
+                        width="853"
+                        height="480"
+                        src="https://my.matterport.com/show/?m=rvKhT7rSPML"
+                        frameborder="0"
+                        allowfullscreen
+                        allow="xr-spatial-tracking"
+                      ></iframe>
+                    ) : (
+                      <iframe
+                        width="853"
+                        height="480"
+                        src={service.img_url}
+                        frameBorder="0"
+                        allowFullScreen
+                        allow="xr-spatial-tracking"
+                      ></iframe>
+                    )}
+                    <GoogleMaps
+                      address={service.address}
+                      city={service.city}
+                      state={service.state}
+                      zipcode={service.zipcode}
+                    />
+                  </div>
+                );
+              })}
           </>
-        </>)
-      }
-    </>)
+        </>
+      )}
+    </>
+  );
 }
